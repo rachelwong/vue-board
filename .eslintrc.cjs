411df671a9732fe08cc5a369a135f7304105a0a1
rachel-wong <hello.rachelwong@gmail.com>
2023-07-04 12:52:30 +1000

    fix: add airbnb linter, clear boilerplate

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,9 +3,9 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
-    'eslint:recommended',
+    '@vue/eslint-config-airbnb',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
